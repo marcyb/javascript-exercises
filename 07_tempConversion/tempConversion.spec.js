0bd4e21873945f93519b9fd5fa6c7ec7541940b6
Marc Bleeze <marcyb@users.noreply.github.com>
2025-02-15 19:17:22 +0000

    Make exercise 7 test 4 💚

--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -7,13 +7,13 @@ describe('convertToCelsius', () => {
   test('rounds to 1 decimal', () => {
     expect(convertToCelsius(100)).toEqual(37.8);
   });
-  test.skip('works with negatives', () => {
+  test('works with negatives', () => {
     expect(convertToCelsius(-100)).toEqual(-73.3);
   });
 });
 
 describe('convertToFahrenheit', () => {
-  test.skip('works', () => {
+  test('works', () => {
     expect(convertToFahrenheit(0)).toEqual(32);
   });
   test.skip('rounds to 1 decimal', () => {
