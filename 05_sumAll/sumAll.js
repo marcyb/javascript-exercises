2b1109e514ca5c168f724ee81601ac5ece38a33f
Marc Bleeze <marcyb@users.noreply.github.com>
2025-02-15 18:46:10 +0000

    Make exercise 5 test 3 💚

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
-const sumAll = function(start, end) {
+const sumAll = function(num1, num2) {
   let sum = 0;
-  for (let i = start; i <= end; i++) {
+  for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
     sum += i;
   }
   return sum;
