56e524c3878a3d5ec23a32f819c8dcaf14d1db30
Marc Bleeze <marcyb@users.noreply.github.com>
2025-02-15 18:19:08 +0000

    Fix for failing test in exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function(str, times) {
-  let output = '';
+const repeatString = function (str, times) {
+  if (times < 0) return "ERROR";
+
+  let output = "";
   for (let i = 0; i < times; i++) {
     output += str;
   }
